d28f775d45308ed2729082b27aaefc4af5a69800
nekoniumoretai <ta.chi.sho.ko@gmail.com>
2024-04-30 00:17:53 +0900

    firstcommit

---
+++ BASIC/main.js
@@ -0,0 +1,8 @@
+let count = 0;
+count = 30;
+let newCount = 0;
+const daysInWeek = 7;
+let string = 'Hello';
+const userName = 'ジャバスクリプト';
+string = `Hello + ${userName}`; //へんすうでuserNameを使いたい場合``で囲わなければならない。
+console.log(string);
